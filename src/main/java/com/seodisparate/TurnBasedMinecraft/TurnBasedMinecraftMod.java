721af3f1d0995758d750819308063c92531f9566
Stephen Seo <seo.disparate@gmail.com>
2018-08-28 11:26:34 +0900

    Init commit with skeleton project of rewrite

---
+++ src/main/java/com/seodisparate/TurnBasedMinecraft/TurnBasedMinecraftMod.java
@@ -0,0 +1,31 @@
+package com.seodisparate.TurnBasedMinecraft;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class TurnBasedMinecraftMod
+{
+    public static final String MODID = "TurnBasedMinecraft";
+    public static final String NAME = "Turn Based Minecraft Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
