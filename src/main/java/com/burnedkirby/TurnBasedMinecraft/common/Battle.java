aad931adb15d419fa5348f67e380774edd14c766
Stephen Seo <seo.disparate@gmail.com>
2026-08-07 16:05:12 +0900

    Apply "weakness" to mobs when attack in battle

--- src/main/java/com/burnedkirby/TurnBasedMinecraft/common/Battle.java
+++ src/main/java/com/burnedkirby/TurnBasedMinecraft/common/Battle.java
@@ -891,6 +891,7 @@ public class Battle {
                                         }
                                         // have player look at attack target
                                         ((ServerPlayer) nextEntity).connection.teleport(nextEntity.getX(), nextEntity.getY(), nextEntity.getZ(), yawDirection, pitchDirection);
+                                        // Do attack
                                         TurnBasedMinecraftMod.proxy.setAttackingEntity(nextEntity);
                                         TurnBasedMinecraftMod.proxy.setAttackingDamage(0);
                                         ((Player) nextEntity).attack(targetEntity);
@@ -972,6 +973,10 @@ public class Battle {
                                         int damageAmount = next.entityInfo.attackPower;
                                         if (next.entityInfo.attackVariance > 0) {
                                             damageAmount += random.nextInt(next.entityInfo.attackVariance * 2 + 1) - next.entityInfo.attackVariance;
+                                        }
+                                        // Apply "weakness" manually
+                                        if (((LivingEntity)next.entity).hasEffect(MobEffects.WEAKNESS)) {
+                                            damageAmount /= 2;
                                         }
                                         if (damageAmount < 0) {
                                             damageAmount = 0;
